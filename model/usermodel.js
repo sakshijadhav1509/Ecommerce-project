4bd203ab96a1b9faa379a79e9f82a40b9632fa30
sakshi <sakshijadhav1509@gmail.com>
2025-07-24 11:11:42 +0530

    first commit

---
+++ model/usermodel.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const userSchema=new mongoose.Schema({
+    username:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    contact:{type:Number},
+    isadmin:{type:Boolean},
+    createdAt:{type:Date,default:Date.now},
+    updateAt:{type:Date,default:Date.now}
+})
+
+module.exports=mongoose.model('users',userSchema)
